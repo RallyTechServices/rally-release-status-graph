a5746ca880828cff997b876917bd3c6f43515920
John Martin <curmudgeon@rallydev.com>
2013-11-12 10:33:14 -0800

    make sure iterations are in order of date

--- src/javascript/_ts-logger.js
+++ src/javascript/_ts-logger.js
@@ -5,27 +5,15 @@ Ext.define('Rally.technicalservices.logger',{
         Ext.apply(this,config);
     },
     log: function(args){
+        var timestamp = "[ " + Ext.util.Format.date(new Date(), "Y-m-d H:i:s.u") + " ]";
+        //var output_args = arguments;
+        //output_args.unshift( [ "[ " + timestamp + " ]" ] );
+        //output_args = Ext.Array.push(output_args,arguments);
+        
         var output_args = [];
-        if (arguments.length === 0 ) {
-            window.console && console.log(arguments);
-            return;
-        }
-        if (arguments.length === 1 ) {
-            output_args = arguments;
-        } else {
-            var src_class = arguments[0];
-            var class_name = "";
-            if ( typeof(src_class) === "string" ) {
-                class_name = src_class;
-            } else if ( typeof src_class.getName === "function") { 
-                class_name = src_class.getName();
-            } else if (typeof src_class.self.getName === 'function'){
-                class_name = src_class.self.getName();
-            }
-            //window.console && console.log(class_name,"--",message);
-            output_args = Ext.Array.push(output_args,[class_name,"--"]);
-            output_args = Ext.Array.push(output_args,Ext.Array.slice(arguments,1));
-        }
+        output_args = Ext.Array.push(output_args,[timestamp]);
+        output_args = Ext.Array.push(output_args, Ext.Array.slice(arguments,0));
+
         window.console && console.log.apply(console,output_args);
     }
 
